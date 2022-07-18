0ffc4f61a76e28b0dba6e52da7eb40d58f589028
denver <dbabiichuk@yandex.ru>
2022-07-18 13:22:15 +0200

    add firewall rules

--- haproxy/haproxy.tf
+++ haproxy/haproxy.tf
@@ -17,3 +17,22 @@ network_interface {
   network = "default"
 }
 }
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = google_compute_network.default.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "1000-2000"]
+  }
+
+  source_tags = ["web"]
+}
+
+resource "google_compute_network" "default" {
+  name = "test-network"
+}
